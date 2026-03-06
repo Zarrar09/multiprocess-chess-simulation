8a2a0ed842c44bf71d5a4a987b8cae10a45451f7
Zarrar09 <zarrarnadeemmalik@gmail.com>
2026-03-06 21:34:44 +0500

    Created the Controller logic, game loop works, game log file created, and pipes and all errors fixed and closed properly

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <unistd.h>
 #include <sys/wait.h>
 using namespace std;
@@ -40,6 +41,45 @@ EXECV vs EXECLP:
     2.  EXECV : You must send parameters through an array
 
 */
+
+bool checkMove(char chessBoard[][8], int oldX, int oldY, int newX, int newY, bool currentTurn)
+{
+    if(!((oldX >= 0 && oldX < 8) && (oldY >= 0 && oldY < 8) && (newX >= 0 && newX < 8) && (newY >= 0 && newY < 8)))
+    {
+        return false;
+    }
+
+    if(chessBoard[oldX][oldY] == '.')
+    {
+        return false;
+    }
+
+    if(currentTurn == true) //white -> Capital letters
+    {
+        if(chessBoard[oldX][oldY] >= 'a' && chessBoard[oldX][oldY] <= 'z')
+        {
+            return false;
+        }
+        if(chessBoard[newX][newY] >= 'A' && chessBoard[newX][newY] <= 'Z') //white piece already present there
+        {
+            return false;
+        }
+    }
+    else if (currentTurn == false) // black -> small letters
+    {
+        if(chessBoard[oldX][oldY] >= 'A' && chessBoard[oldX][oldY] <= 'Z')
+        {
+            return false;
+        }
+        if(chessBoard[newX][newY] >= 'a' && chessBoard[newX][newY] <= 'z') //black piece already present there
+        {
+            return false;
+        }
+    }
+
+    //if all above passes we return true
+    return true;
+}
 
 void createChessBoard(char board[8][8])
 {
@@ -114,6 +154,7 @@ void printBoard(char board[8][8])
 int main()
 {
     char chessBoard[8][8];
+    int maxMoves = 100;
 
     createChessBoard(chessBoard);
     printBoard(chessBoard);
@@ -198,7 +239,127 @@ int main()
             close(black_controller[1]);
 
             // game loop begins
-            write(controller_white[1], &chessBoard, 64);
+
+            int totalMoves = 0;
+            bool currentTurn = true;
+            bool gameOver = false;
+
+            ofstream logFile("game_log.txt");
+
+            while(totalMoves < maxMoves && !gameOver)
+            {
+                if(currentTurn == true) // white turn
+                {
+                    sleep(3);
+
+                    write(controller_white[1], &chessBoard, 64);
+
+                    char move[6] = {0};
+                    read(white_controller[0], move, 5);
+
+                //E2E4
+                // Letter converted by doing 'E' - 'A'
+                // Number: Convert '2' to intger and 8 - that integer
+                // chessMaxCol - colIndex = chessYCoordinate
+                // chessMaxCol - chessYCoordinate = colIndex -> we are doing this rn
+
+                    int originalX = move[0] - 'A';
+                    int originalY = 8 - (move[1] - '0');
+                    int newX = move[2] - 'A';
+                    int newY = 8 - (move[3] - '0');
+
+                    if(checkMove(chessBoard, originalY, originalX, newY, newX, currentTurn))
+                    {
+                        char captured = chessBoard[newY][newX];
+
+                        char piece = chessBoard[originalY][originalX];
+
+                        chessBoard[originalY][originalX] = '.';
+
+                        chessBoard[newY][newX] = piece;
+
+                        totalMoves++;
+
+                        cout << "| Move Count: " << totalMoves << " |  PID " << pid << " | " << "Move: "<< move[0] << move[1] << move[2] << move[3] << " | " << endl;
+                        logFile << "| Move Count:" << totalMoves << "| PID " << pid << "Move: "<< move[0] << move[1] << move[2] << move[3] << " | " << "\n";
+
+                        printBoard(chessBoard);
+
+                        if(captured == 'k')
+                        {
+                            cout << "White wins | Black King Captured" << endl;
+                            logFile << "White wins | Black king captured.\n";
+                            gameOver = true;
+                        }
+
+                        currentTurn = !currentTurn;
+                    }
+                    else
+                    {
+                        cout << "Illegal move by White | " << "Move: "<< move[0] << move[1] << move[2] << move[3] << " | "<< endl;
+                        logFile << "Illegal move by White | " << "Move: "<< move[0] << move[1] << move[2] << move[3] << " | "<< "\n";
+                        gameOver = true;
+                    }
+                }
+                else // black turn
+                {
+                    
+                    sleep(3);
+                    write(controller_black[1], &chessBoard, 64);
+
+                    char move[6] = {0};
+                    read(black_controller[0], move, 5);
+
+                    int originalX = move[0] - 'A';
+                    int originalY = 8 - (move[1] - '0');
+                    int newX = move[2] - 'A';
+                    int newY = 8 - (move[3] - '0');
+
+                    if(checkMove(chessBoard, originalY, originalX, newY, newX, currentTurn))
+                    {
+                        char captured = chessBoard[newY][newX];
+                        char piece = chessBoard[originalY][originalX];
+                        chessBoard[originalY][originalX] = '.';
+                        chessBoard[newY][newX] = piece;
+                        totalMoves++;
+
+                        cout << "| Move Count: " << totalMoves << " |  PID " << pid2 << " | Move: " << move[0] << move[1] << move[2] << move[3] << " | " << endl;
+                        logFile << "| Move Count:" << totalMoves << "| PID " << pid2 << " | Move: "<< move[0] << move[1] << move[2] << move[3] << " | " << "\n";
+
+                        printBoard(chessBoard);
+
+                        if(captured == 'K')
+                        {
+                            cout << "Black wins | White king captured." << endl;
+                            logFile << "Black wins | White king captured.\n";
+                            gameOver = true;
+                        }
+
+                        currentTurn = !currentTurn;
+                    }
+                    else
+                    {
+                        cout << "Illegal move by Black | " << move[0] << move[1] << move[2] << move[3] << " | "<< endl;
+                        logFile << "Illegal move by Black | " << move[0] << move[1] << move[2] << move[3] << " | " << "\n";
+                        gameOver = true;
+                    }
+                }
+            }
+
+            if(!gameOver && totalMoves >= maxMoves)
+            {
+                cout << "Draw -> " << maxMoves << "moves reached." << endl;
+                logFile << "Draw -> " << maxMoves << " moves reached.\n";
+            }
+
+            // by closing the write ends we sends EOF to both players, so then the game closes
+            close(controller_white[1]);
+            close(controller_black[1]);
+
+            logFile.close();
+
+            waitpid(pid, NULL, 0);
+            waitpid(pid2, NULL, 0);
         }
     }
 
